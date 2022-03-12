1805e3c7fa58965367601c7ac7637598ff2a43f9
Özay Akcan <iletisim@ozayakcan.com.tr>
2022-03-13 02:28:18 +0300

    Gereksiz importlar kaldırıldı.

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-
-import 'package:auth_buttons/auth_buttons.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
