ebd0792f813c6f65736fe36c55653a330d6b54b9
Özay Akcan <iletisim@ozayakcan.com.tr>
2022-03-29 10:15:30 +0300

    Profil resmi görüntüleme artık cached_network_image ile yapılıyor.

--- lib/widgets/images.dart
+++ lib/widgets/images.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:sosyal/utils/assets.dart';
 
@@ -14,11 +15,25 @@ Widget defaultProfileImage({double width = 100, double height = 100}) {
 }
 
 Widget profileImage(String path, {double width = 100, double height = 100}) {
-  return SizedBox(
-    width: width,
-    height: height,
-    child: CircleAvatar(
-      backgroundImage: NetworkImage(path),
+  return CachedNetworkImage(
+    imageUrl: path,
+    imageBuilder: (context, imageProvider) => Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+      ),
     ),
+    placeholder: (context, url) => defaultProfileImage(
+      width: width,
+      height: height,
+    ),
+    errorWidget: (context, url, error) => defaultProfileImage(
+      width: width,
+      height: height,
+    ),
+    fadeInDuration: Duration.zero,
+    fadeOutDuration: Duration.zero,
   );
 }
